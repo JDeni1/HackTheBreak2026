35bccfdd1fb82bbb7ac298a7d71d4ad224846073
Jill <jbautista19@my.bcit.ca>
2026-03-14 16:39:31 -0700

    Saving my things

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-// const express = require("express");
-const port = process.env.PORT || 8080;
 const express = require("express");
-const app = express();
 const path = require("path");
 
-app.use(express.static(__dirname));
+const app = express();
+const PORT = process.env.PORT || 8080;
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "main.html"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "main.html"));
 });
 
-app.listen(process.env.PORT || 8080);
-console.log("Server is running on port ${process.env.PORT}");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
